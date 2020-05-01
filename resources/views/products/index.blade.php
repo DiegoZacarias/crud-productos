8f0e9edb1135fe0ddbbff374012107ecdae91642
DiegoZacarias <diegozaca97@gmail.com>
2020-04-30 23:30:38 -0400

    Quinto commit, arreglo visual de la tabla producto

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="col-md-12">
         <div class="card">
             <div class="card-header">Productos
                 <a href=" {{ route('products.create') }}  " class="btn btn-sm btn-primary" >Crear</a>
